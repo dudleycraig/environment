046ba308b6d16af8ce7e06b769b42ac3ff885cf0
dudleycraig <dudleycraig@gmail.com>
2018-05-30 10:24:55 +0200

    cleaned up script, fixed figwheel issue

--- clojure.env.sh
+++ clojure.env.sh
@@ -1,44 +1,59 @@
 #!/usr/bin/bash
-ROOT=${1:-"~/workspace"}
+{
+   ROOT=${1:-"~/workspace"}
 
-function editor-window {
+   tmux start-server 
+   tmux new-session -d -s clojure
+
    tmux rename-window editor 
 
    tmux select-pane -t 0 
-   tmux send-keys "cd ${ROOT} && clear" C-m
    tmux split-window -v   
-
    tmux select-pane -t 1 
-   tmux send-keys 'export JAVA_CMD="${JAVA_HOME}/bin/java.exe"' C-m 
-   tmux send-keys "cd ${ROOT} && clear" C-m
    tmux split-window -h
-
-   tmux select-pane -t 2 
-   tmux send-keys 'export JAVA_CMD="${JAVA_HOME}/bin/java.exe"' C-m 
-   tmux send-keys "cd ${ROOT} && clear" C-m
-
-   tmux select-pane -t 1
+   tmux select-pane -t 1 
    tmux split-window -v
-
    tmux select-pane -t 3 
-   tmux send-keys 'export JAVA_CMD="$(cygpath -w ${LEIN_JAVA_CMD})"' C-m 
-   tmux send-keys "cd ${ROOT} && clear" C-m
    tmux split-window -v
+
+   tmux select-pane -t 0 
+   tmux send-keys "cd ${ROOT}" C-m
+   tmux send-keys "clear" C-m
+   tmux select-pane -t 1 
+   tmux send-keys "cd ${ROOT}" C-m
+   tmux send-keys "clear" C-m
+   tmux select-pane -t 2 
+   tmux send-keys "cd ${ROOT}" C-m
+   tmux send-keys "clear" C-m
+   tmux select-pane -t 3 
+   tmux send-keys "cd ${ROOT}" C-m
+   tmux send-keys "clear" C-m
+   tmux select-pane -t 4 
+   tmux send-keys "cd ${ROOT}" C-m
+   tmux send-keys "clear" C-m
 
    tmux select-pane -t 4 
    tmux send-keys 'export JAVA_CMD="$(cygpath -w ${LEIN_JAVA_CMD})"' C-m 
-   tmux send-keys "cd ${ROOT} && clear" C-m
-}
+   tmux send-keys "clear" C-m
 
-{
-   tmux -vvv start-server 
-   tmux new-session -d -s clojure
+   tmux select-pane -t 3 
+   tmux send-keys 'export JAVA_CMD="$(cygpath -w ${LEIN_JAVA_CMD})"' C-m 
+   tmux send-keys "clear" C-m
 
-   # tmux source-file ~/.tmux/clojure.env
-   editor-window
+   tmux select-pane -t 2 
+   # tmux send-keys 'export JAVA_CMD="${JAVA_HOME}/bin/java.exe"' C-m # fix for when figwheel can't find java within windows path
+   tmux send-keys 'export JAVA_CMD="$(cygpath -w ${LEIN_JAVA_CMD})"' C-m 
+   tmux send-keys "clear" C-m
 
-   select-pane -t 0 
-   attach-session -t clojure
+   tmux select-pane -t 1 
+   # tmux send-keys 'export JAVA_CMD="${JAVA_HOME}/bin/java.exe"' C-m # fix for when figwheel can't find java within windows path
+   tmux send-keys 'export JAVA_CMD="$(cygpath -w ${LEIN_JAVA_CMD})"' C-m 
+   tmux send-keys "clear" C-m
+
+   tmux select-pane -t 0 
+   tmux send-keys 'export JAVA_CMD="$(cygpath -w ${LEIN_JAVA_CMD})"' C-m 
+   tmux send-keys "clear" C-m
 
    tmux attach-session -t clojure
 }
+
